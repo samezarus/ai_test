f41b1de54f0d4e9c26bdf39634b842d2cbb745e9
sameza <samezarus@gmail.com>
2025-08-31 19:06:48 +1000

    upd

---
+++ ollama_scan_img/test1/main.py
@@ -0,0 +1,44 @@
+# pip install langchain langchain-community ollama
+# pip install langchain-core
+
+
+import base64
+from langchain.chat_models import ChatOpenAI  # или аналогичный класс
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage
+from pathlib import Path
+
+# кодируем изображение
+img_bytes = Path("skan.jpg").read_bytes()
+img_b64 = base64.b64encode(img_bytes).decode("utf-8")
+
+# формируем сообщение
+prompt = """
+# Ты мультимодальный AI-агент для работы со сканированными документами на русском языке.  
+# Задача: проанализировать изображение и вернуть данные в формате JSON со следующими ключами:
+- text: извлечённый текст (или пустая строка, если текста нет)
+- summary: краткое содержание текста (или пустая строка, если текста нет)
+- tables: список таблиц в markdown-нотации (или пустой массив, если таблиц нет)
+# Замечания:
+- Не придумывай данные, если не смог распознать их на изображении
+- Соблюдай чёткую посделовательность данных в тексте
+"""
+
+message = HumanMessage(
+    content=[
+        {
+            "type": "text", 
+            "text": prompt
+        },
+        {
+            "type": "image", 
+            "source_type": "base64", 
+            "mime_type": "image/jpg", 
+            "data": img_b64
+        },
+    ]
+)
+
+llm = ChatOllama(model="qwen2.5vl:3b", base_url="http://localhost:11434")
+response = llm.invoke([message])
+print(response.content)
